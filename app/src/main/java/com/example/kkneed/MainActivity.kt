5d6fe0903c43cb24ec8dc787424bce3ae7c308a4
devenv's home <devenv_1390@outlook.com>
2023-01-25 20:58:41 +0800

    change bottom navigation and Color.kt and Theme.kt

--- app/src/main/java/com/example/kkneed/MainActivity.kt
+++ app/src/main/java/com/example/kkneed/MainActivity.kt
@@ -4,10 +4,8 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -15,11 +13,13 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.kkneed.ui.theme.KKNeedTheme
+import com.example.kkneed.ui.theme.*
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -28,15 +28,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             KKNeedTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                    elevation = 10.dp,
+                ) {
                     Sample()
                 }
             }
         }
     }
 }
+
 data class Item(
-    val index:Int,
+    val index: Int,
     val name: String,
     val icon: ImageVector,
 )
@@ -60,12 +65,29 @@ fun MySnackBar(snackbarHostState: SnackbarHostState, onDismiss: () -> Unit) {
 fun MyBottomNavigation() {//第二种设计方案，采用BottomAppBar与BottomNavigationItem结合的方式，解决了选项卡按下后没有外观变化的缺点，但动画和按键的布局范围比较难控制
     var selectedItem by remember { mutableStateOf(0) }
     val items = listOf(
-        Item(0,"主页", icon = Icons.Default.Home),
-        Item(1,"商城", icon = Icons.Default.ShoppingCart),
-        Item(2,"记录", icon = Icons.Default.Check),
-        Item(3,"个人", icon = Icons.Default.Person)
+        Item(0, "主页", icon = Icons.Default.Home),
+        Item(1, "商城", icon = Icons.Default.ShoppingCart),
+        Item(2, "记录", icon = Icons.Default.Check),
+        Item(3, "个人", icon = Icons.Default.Person)
     )
-    BottomAppBar(cutoutShape = RoundedCornerShape(50), contentPadding = PaddingValues(5.dp,5.dp)) {
+    Surface(
+        shape = RoundedCornerShape(8.dp, 8.dp, 0.dp, 0.dp),
+        modifier = Modifier
+            .shadow(
+                elevation = 20.dp,
+                ambientColor = Color.Black
+            )
+            .fillMaxWidth()
+            .size(412.dp, 60.dp)
+    ) {
+        BottomAppBar(
+            cutoutShape = RoundedCornerShape(50),
+            contentPadding = PaddingValues(5.dp, 5.dp),
+            backgroundColor = Color.White,
+//            modifier = Modifier.shadow(
+//                elevation = 15.dp,
+//            )
+        ) {
 //        items.forEachIndexed{index, item ->
 //            BottomNavigationItem(
 //                selected = selectedItem == index,
@@ -75,39 +97,42 @@ fun MyBottomNavigation() {//第二种设计方案，采用BottomAppBar与BottomN
 //                label = { Text(item.name)},
 //            )
 //        }//不能采用遍历的方式创建导航选项卡，因为选项卡需要单独设置间隔值
-        BottomNavigationItem(
-            selected = selectedItem == 0,
-            onClick = {selectedItem = 0},
-            icon = { Icon(items[0].icon,null) },
-            alwaysShowLabel = false,
-            label = { Text(items[0].name)},
-            modifier = Modifier.padding(10.dp,0.dp,10.dp,0.dp)
-        )
-        BottomNavigationItem(
-            selected = selectedItem == 1,
-            onClick = {selectedItem = 1},
-            icon = { Icon(items[1].icon,null) },
-            alwaysShowLabel = false,
-            label = { Text(items[1].name)},
-            modifier = Modifier.padding(0.dp,0.dp,40.dp,0.dp)
-        )
-        BottomNavigationItem(
-            selected = selectedItem == 2,
-            onClick = {selectedItem = 2},
-            icon = { Icon(items[2].icon,null) },
-            alwaysShowLabel = false,
-            label = { Text(items[2].name)},
-            modifier = Modifier.padding(40.dp,0.dp,0.dp,0.dp)
-        )
-        BottomNavigationItem(
-            selected = selectedItem == 3,
-            onClick = {selectedItem = 3},
-            icon = { Icon(items[3].icon,null) },
-            alwaysShowLabel = false,
-            label = { Text(items[3].name)},
-            modifier = Modifier.padding(10.dp,0.dp,10.dp,0.dp)
-        )
+            BottomNavigationItem(
+                selected = selectedItem == 0,
+                onClick = { selectedItem = 0 },
+                icon = { Icon(items[0].icon, null) },
+                alwaysShowLabel = false,
+                label = { Text(items[0].name) },
+                modifier = Modifier.padding(10.dp, 0.dp, 10.dp, 0.dp)
+            )
+            BottomNavigationItem(
+                selected = selectedItem == 1,
+                onClick = { selectedItem = 1 },
+                icon = { Icon(items[1].icon, null) },
+                alwaysShowLabel = false,
+                label = { Text(items[1].name) },
+                modifier = Modifier.padding(0.dp, 0.dp, 40.dp, 0.dp)
+            )
+            BottomNavigationItem(
+                selected = selectedItem == 2,
+                onClick = { selectedItem = 2 },
+                icon = { Icon(items[2].icon, null) },
+                alwaysShowLabel = false,
+                label = { Text(items[2].name) },
+                modifier = Modifier.padding(40.dp, 0.dp, 0.dp, 0.dp)
+            )
+            BottomNavigationItem(
+                selected = selectedItem == 3,
+                onClick = { selectedItem = 3 },
+                icon = { Icon(items[3].icon, null) },
+                alwaysShowLabel = false,
+                label = { Text(items[3].name) },
+                modifier = Modifier.padding(10.dp, 0.dp, 10.dp, 0.dp)
+            )
+        }
     }
+
+
 }
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -117,41 +142,33 @@ fun Sample() {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
     Scaffold(
+        backgroundColor = md_theme_light_background,
         snackbarHost = {
             scaffoldState.snackbarHostState
         },
-        topBar = {
-            TopAppBar(
-                title = { Text("主页") },
-                navigationIcon = {
-                    IconButton(onClick = {}) {
-                        Icon(Icons.Filled.Menu, null)
-                    }
-                }
-            )
-        },
         bottomBar = {
             MyBottomNavigation()
-//            MyBottomAppBar()
         },
         isFloatingActionButtonDocked = true,
         floatingActionButtonPosition = FabPosition.Center,
         floatingActionButton = {
-            FloatingActionButton(onClick = {
-                scope.launch {
-                    scaffoldState.snackbarHostState.showSnackbar(
-                        message = "扫码功能"
-                    )
-                }
-            }
+            FloatingActionButton(
+                onClick = {
+                    scope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar(
+                            message = "扫码功能"
+                        )
+                    }
+                },
+                backgroundColor = md_theme_dark_primary
             ) {
-                Icon(Icons.Default.Build, null, tint = Color.Black)
+                Icon(Icons.Default.Build, null, tint = Color.White)
             }
         }
     ) {
         Box(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            contentAlignment = Alignment.Center,
         ) {
             MySnackBar(
                 snackbarHostState = scaffoldState.snackbarHostState,
