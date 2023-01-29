b5a85ddc6c5f8cc9077bdd0ec6fe3a73d419c45e
devenv's home <devenv_1390@outlook.com>
2023-01-29 22:18:27 +0800

    完成个人主页一级页面UI

--- app/src/main/java/com/example/kkneed/model/ItemData.kt
+++ app/src/main/java/com/example/kkneed/model/ItemData.kt
@@ -1,7 +1,14 @@
 package com.example.kkneed.model
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 data class GuideScreenItemData (//引导页的item数据类
     val image: Int,
     val title: String,
     val content: String,
+)
+data class ButtomItemData(
+    val image: ImageVector,
+    val title: String,
+    val route: String
 )
--- app/src/main/java/com/example/kkneed/screen/WelcomeScreen.kt
+++ app/src/main/java/com/example/kkneed/screen/WelcomeScreen.kt
@@ -2,29 +2,38 @@ package com.example.kkneed.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.kkneed.navigation.AllScreen
+import com.example.kkneed.ui.MyTopAppBar
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun WelcomeScreen(navController: NavController) {
     Scaffold(
-        backgroundColor = MaterialTheme.colorScheme.background
+        backgroundColor = MaterialTheme.colorScheme.background,
+        topBar = { MyTopAppBar { Text("欢迎界面") }}
     ) {
-        Box(modifier = Modifier.fillMaxWidth()){
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ){
+            Spacer(modifier = Modifier.padding(0.dp, 10.dp))
             Button(onClick = {
                 navController.navigate(AllScreen.Guide.route)
             }){
                 Text("欢迎！")
             }
         }
-        Text("欢迎界面")
     }
 }
