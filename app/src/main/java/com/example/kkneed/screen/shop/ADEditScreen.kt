5b3496b834dcd3757869c687804b34816f570801
Wukexin <wukexin@gmail.com>
2023-02-22 21:07:16 +0800

    修复merge过程中的错误

--- app/src/main/java/com/example/kkneed/screen/shop/ADEditScreen.kt
+++ app/src/main/java/com/example/kkneed/screen/shop/ADEditScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.kkneed.ui.ADListCard
 import com.example.kkneed.ui.GradientButton
 import com.example.kkneed.ui.MostUseTopAppBar
 import com.example.kkneed.ui.theme.KKNeedTheme
